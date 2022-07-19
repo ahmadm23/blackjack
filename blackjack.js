06c6a907aaed36745e3121e7f769d07ba0d44823
ahmadm23 <93296021+ahmadm23@users.noreply.github.com>
2022-07-19 19:51:44 -0400

    Add files via upload

--- blackjack.js
+++ blackjack.js
@@ -54,7 +54,68 @@ function startGame(){
         dealerAceCount += checkAce(card);
         document.getElementById("dealer-cards").append(cardImg);
     }
+    console.log(dealerSum);
+
+    for (let i = 0; i < 2; i++){
+        let cardImg = document.createElement("img");
+        let card = deck.pop();
+        cardImg.src = "../cards/" + card + ".png";
+        yourSum += getValue(card);
+        yourAceCount += checkAce(card);
+        document.getElementById("your-cards").append(cardImg);
+    }
+    console.log(yourSum);
+    document.getElementById("hit").addEventListener("click", hit);
+    document.getElementById("stay").addEventListener("click", stay);
 }
+
+function stay(){
+    dealerSum = reduceAce(dealerSum,dealerAceCount);
+    yourSum = reduceAce(yourSum,yourAceCount);
+
+    canHit = false;
+    document.getElementById("hidden").src = "../cards/" + hidden + ".png";
+
+    let message = "";
+    if (yourSum > 21){
+        message = "You Lose!";
+    }
+    else if (dealerSum > 21){
+        message = "You Win!";
+    }
+    //both have <= 21
+    else if (yourSum == dealerSum){
+        message = "Tie!";
+    }
+    else if (yourSum > dealerSum){
+        message = "You Win!";
+    }
+    else if (yourSum < dealerSum){
+        message = "You Lose!";
+    }
+    document.getElementById("dealer-sum").innerText = dealerSum;
+    document.getElementById("your-sum").innerText = yourSum;
+    document.getElementById("results").innerText = message;
+
+}
+
+function hit(){
+    if (!canHit){
+        return;
+    }
+    let cardImg = document.createElement("img");
+    let card = deck.pop();
+    cardImg.src = "../cards/" + card + ".png";
+    yourSum += getValue(card);
+    yourAceCount += checkAce(card);
+    document.getElementById("your-cards").append(cardImg);
+
+    if (reduceAce(yourSum, yourAceCount) > 21){
+        canHit = false;
+    }
+}
+
+
 
 function getValue(card){
     let data = card.split("-"); //dash spits "4-C" -> ["4, C"]
@@ -76,3 +137,11 @@ function checkAce(card){
     return 0;
 }
 
+function reduceAce(playerSum, playerAceCount){
+    while (playerSum > 21 && playerAceCount > 0){
+        playerSum -= 10;
+        playerAceCount -= 1;
+    }
+    return playerSum;
+
+}
